7378fdd9a0f4c12b9fe62b0021529c948c8048c4
SamtasticHan <tghan77@icloud.com>
2025-11-01 23:01:51 +0900

    Update account number for bride's bank

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ function copyAccount(type) {
   if (type === '신랑') {
     account = "신랑 한상우 / 카카오뱅크 3333-30-2663222";
   } else {
-    account = "신부 안은정 / 국민은행 123456-78-901234";
+    account = "신부 안은정 / 카카오뱅크 3333-01-4974626";
   }
 
   navigator.clipboard.writeText(account);
   document.getElementById("accountMsg").innerText = `${type}측 계좌번호가 복사되었습니다.`;
 }
+
 
 
 
